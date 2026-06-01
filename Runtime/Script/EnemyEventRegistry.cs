b83fe05b945871052afe3e69b51908774c62d1ff
vegahouse102 <62749830+vegahouse102@users.noreply.github.com>
2026-06-02 00:51:33 +0900

    Add event Registry

--- Runtime/Script/EnemyEventRegistry.cs
+++ Runtime/Script/EnemyEventRegistry.cs
@@ -1,14 +1,55 @@
+
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 
 namespace Enemy
 {
 	public class EnemyEventRegistry : MonoBehaviour
 	{
-		public void ExecuteEvent(string eventName)
-		{
+		[SerializeField]
+		private AnimationEventRelay _animationEventRelay;
 
+		[SerializeField]
+		private List<EventContainer> _eventContainers = new();
+
+		private Dictionary<string, UnityEvent> _eventCache = new();
+
+
+		private void Awake()
+		{
+			foreach(var container in _eventContainers)
+			{
+				_eventCache.Add(container.EventName, container.Events);
+			}
+			_animationEventRelay.OnTriggerEvent += ExecuteEvent;
+		}
+
+
+		private void OnDestroy()
+		{
+			_animationEventRelay.OnTriggerEvent -= ExecuteEvent;
+		}
+		
+
+		private void ExecuteEvent(string eventName)
+		{
+			if(_eventCache.TryGetValue(eventName,out UnityEvent unityEvent)){
+				unityEvent?.Invoke();
+			}
+		}
+
+
+		[Serializable]
+		public  class EventContainer
+		{
+			public string EventName;
+			public UnityEvent Events;
 		}
 	}
+
+	
 }
 
