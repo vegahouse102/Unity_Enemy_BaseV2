efae97228d810199a96c19f1c1498eea38d9225c
vegahouse102 <62749830+vegahouse102@users.noreply.github.com>
2026-06-02 19:03:51 +0900

    Update SimpleAnimationBehaviour.cs

--- Runtime/Script/Behaviour/SimpleAnimationBehaviour.cs
+++ Runtime/Script/Behaviour/SimpleAnimationBehaviour.cs
@@ -12,8 +12,12 @@ namespace Enemy
 		Animator _animator;
 		[SerializeField]
 		AnimationEventRelay _animationEventBroadcaster;
+
+		[Tooltip("animation트리거타입 Trigger or Bool")]
 		[SerializeField]
-		[Tooltip("Animation을 끝낼 trigger이름")]
+		AnimationType _animationType = AnimationType.Trigger;
+		[SerializeField]
+		[Tooltip("Animation을 시작할 trigger이름")]
 		private string _triggerName;
 		[SerializeField]
 		[Tooltip("AnimationEventBroadcaster를 이용해 animator에 이벤트를 붙일때 실행할 이름")]
@@ -35,7 +39,12 @@ namespace Enemy
 		protected override Node.Status OnStartProcess()
 		{
 			_isDone = false;
-			_animator.SetTrigger(_triggerName);
+			if(_animationType == AnimationType.Trigger)
+				_animator.SetTrigger(_triggerName);
+			else
+			{
+				_animator.SetBool(_triggerName,true);
+			}
 			return Node.Status.Running;
 		}
 
@@ -47,7 +56,10 @@ namespace Enemy
 		}
 		protected override void OnEndProcess()
 		{
-
+			if(_animationType == AnimationType.Bool)
+			{
+				_animator.SetBool(_triggerName, false);
+			}
 		}
 
 		public void OnAnimationDone(string eventName)
@@ -55,7 +67,11 @@ namespace Enemy
 			if(eventName==_animationDoneEventName)
 				_isDone = true;
 		}
-
+		private enum AnimationType
+		{
+			Bool,
+			Trigger
+		}
 	}
 }
 
