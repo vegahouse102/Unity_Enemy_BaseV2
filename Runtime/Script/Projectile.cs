4aa906a0f48fa0d3281df0b01ae3a0b0a15d8f14
vegahouse102 <62749830+vegahouse102@users.noreply.github.com>
2026-06-04 15:03:18 +0900

    Modify Projectile

--- Runtime/Script/Projectile.cs
+++ Runtime/Script/Projectile.cs
@@ -6,11 +6,6 @@ namespace Enemy
 
 	public class Projectile : MonoBehaviour
 	{
-		[SerializeField]
-		Rigidbody2D _rigid;
-
-		[SerializeField]
-		Vector2 _velocity;
 
 		[SerializeField,Min(0)]
 		private float _removeTime = 7f;
@@ -18,26 +13,17 @@ namespace Enemy
 		private float _startTime;
 		private void Awake()
 		{
-#if UNITY_EDITOR
-			Debug.Assert( _rigid != null );
-			Debug.Assert( _velocity != null );
-#endif
 			_startTime = Time.time;
 		}
 
 		// Update is called once per frame
 		private void Update()
 		{
-			_rigid.linearVelocity = _velocity;
 
 			if(Time.time >= _startTime + _removeTime)
 			{
 				Destroy(this.gameObject);
 			}
-		}
-		public void SetVelocity(Vector2 velocity)
-		{
-			_velocity= velocity;
 		}
 	}
 
