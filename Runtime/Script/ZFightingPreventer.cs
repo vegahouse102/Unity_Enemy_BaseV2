e411e02636d394e54817ffa741f116df3e4ea7e7
vegahouse102 <62749830+vegahouse102@users.noreply.github.com>
2026-06-05 22:09:33 +0900

    Update ZFightingPreventer.cs

--- Runtime/Script/ZFightingPreventer.cs
+++ Runtime/Script/ZFightingPreventer.cs
@@ -4,11 +4,16 @@ namespace Enemy
 {
 	public class ZFightingPreventer : MonoBehaviour
 	{
+		private float _baseZPos;
+		private void Awake()
+		{
+			_baseZPos = transform.position.z;
+		}
 		private void OnEnable()
 		{
 			Vector3 pos = transform.position;
 			float offset = Random.Range(-0.01f, -0.0001f);
-			pos.z += offset;
+			pos.z = _baseZPos+offset;
 			transform.position = pos;
 		}
 	}
