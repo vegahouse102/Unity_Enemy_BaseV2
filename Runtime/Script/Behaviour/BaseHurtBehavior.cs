f8e5070925e6bd2816c232f23e511d2257b394f0
vegahouse102 <62749830+vegahouse102@users.noreply.github.com>
2026-06-30 02:42:16 +0900

    hit bug

--- Runtime/Script/Behaviour/BaseHurtBehavior.cs
+++ Runtime/Script/Behaviour/BaseHurtBehavior.cs
@@ -8,13 +8,12 @@ namespace Enemy
 		[SerializeField]
 		private SpriteRenderer _spriteRenderer;
 		[SerializeField]
-		private float _blinkTime;
+		private Color _hitColor;
 		[SerializeField]
-		private float _blinkSpeed;
+		private float _blinkTime;
 
 		private float _curTime;
-		private float _curBlinkTime;
-		private bool _isblink;
+
 		private void Awake()
 		{
 #if UNITY_EDITOR
@@ -24,8 +23,7 @@ namespace Enemy
 		protected override Node.Status OnStartProcess()
 		{
 			_curTime = 0f;
-			_curBlinkTime = 0f;
-			_isblink = false;
+			_spriteRenderer.color = _hitColor;
 			return Node.Status.Running;
 		}
 
@@ -33,21 +31,7 @@ namespace Enemy
 		{
 			if (_curTime > _blinkTime)
 				return Node.Status.Success;
-			if( _curBlinkTime >= _blinkSpeed)
-			{
-				_curBlinkTime = 0f;
-				_isblink = !_isblink;
-				if (_isblink)
-				{
-					_spriteRenderer.color = Color.red;
-				}
-				else
-				{
-					_spriteRenderer.color = Color.white;
-				}
-			}
 			_curTime += Time.deltaTime;
-			_curBlinkTime += Time.deltaTime;
 			return Node.Status.Running;
 		}
 
